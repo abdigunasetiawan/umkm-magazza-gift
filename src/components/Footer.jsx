9bfa243ca6b5f3013ef16f18c1a62b7549f97565
abdigunasetiawan <abdigunasetiawan0@gmail.com>
2025-06-06 21:31:28 +0700

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="border-t border-gray-200 bg-white py-8 text-center font-medium dark:border-gray-600 dark:bg-gray-900 dark:text-white">
-      Copyright 2025. Made by STMIK Kaputama students
+      Made with ❤️ by{" "}
+      <a href="https://abdi-dev.vercel.app" className="font-bold">
+        Abdi Dev
+      </a>{" "}
+      and Team.
     </div>
   );
 };
